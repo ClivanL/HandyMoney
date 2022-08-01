27205aa9ff48b95d895b33790f30a272cdfb507f
Clivan Lee <clivan93@gmail.com>
2022-08-01 13:29:39 +0800

    Revert "Update views.py"
    This reverts commit 269e8183caa759b37084f6e978c9e264d2826a84.

--- expenses/views.py
+++ expenses/views.py
@@ -6,22 +6,21 @@ from rest_framework.response import Response
 
 # Create your views here.
 class PersonViewSet(viewsets.ModelViewSet):
-    queryset=Person.objects.all().order_by('id')
+    queryset=Person.objects.all()
     serializer_class=PersonSerializer
     permission_classes=[permissions.AllowAny]
-    
 
 class ReceiptViewSet(viewsets.ModelViewSet):
-    queryset=Receipt.objects.all().order_by('id')
+    queryset=Receipt.objects.all()
     serializer_class=ReceiptSerializer
     permission_classes=[permissions.AllowAny]
 
 class ItemViewSet(viewsets.ModelViewSet):
-    queryset=Item.objects.all().order_by('id')
+    queryset=Item.objects.all()
     serializer_class=ItemSerializer
     permission_classes=[permissions.AllowAny]
 
 class PartyViewSet(viewsets.ModelViewSet):
-    queryset=Party.objects.all().order_by('id')
+    queryset=Party.objects.all()
     serializer_class=PartySerializer
     permission_classes=[permissions.AllowAny]
